7559ee678ceff6227c244b7ceccb1c0fa90df337
Matthew D Jones <matthew.d.jones@tessella.com>
2020-12-19 12:55:07 +0000

    create NeXus data sources

--- create_data_sources_from_nexus.py
+++ create_data_sources_from_nexus.py
@@ -1,18 +1,17 @@
 import h5py
-import numpy as np
-from os.path import join
-from typing import Union, List, Dict
+from typing import Union, Tuple, Dict, List
+from data_source import DataSource, LogDataSource, EventDataSource
 
 
 def find_by_nx_class(
-        nx_class_names: List[str],
+        nx_class_names: Tuple[str, ...],
         root: Union[h5py.File, h5py.Group]) -> Dict[str, h5py.Group]:
     groups_with_requested_nx_class = {
         class_name: []
         for class_name in nx_class_names
     }
 
-    def _match_nx_class(name, h5_object):
+    def _match_nx_class(_, h5_object):
         if isinstance(h5_object, h5py.Group):
             try:
                 if h5_object.attrs["NX_class"].decode(
@@ -26,5 +25,11 @@ def find_by_nx_class(
     return groups_with_requested_nx_class
 
 
-def find_data_sources_in_nexus_file():
-    pass
+def create_data_sources_from_nexus_file(nexus_file: h5py.File) -> List[DataSource]:
+    nx_log = "NXlog"
+    nx_event_data = "NXevent_data"
+    groups = find_by_nx_class((nx_log, nx_event_data), nexus_file)
+    data_sources: List[DataSource]
+    data_sources = [LogDataSource(group) for group in groups[nx_log]]
+    data_sources.extend([EventDataSource(group) for group in groups[nx_event_data]])
+    return data_sources
--- hdf5_data_source.py
+++ hdf5_data_source.py
@@ -1,23 +1,24 @@
 from data_chunk import LogDataChunk, EventDataChunk
+import h5py
 
 
 class LogDataSource:
-    def __init__(self):
+    def __init__(self, group: h5py.Group):
         """
         Load data, one chunk at a time from NXlog in NeXus file
         """
-        pass
+        self._group = group
 
     def get_data(self) -> (LogDataChunk, int):
         pass
 
 
 class EventDataSource:
-    def __init__(self):
+    def __init__(self, group: h5py.Group):
         """
         Load data, one pulse at a time from NXevent_data in NeXus file
         """
-        pass
+        self._group = group
 
     def get_data(self) -> (EventDataChunk, int):
         pass
