fix unit conversion

nexus_streamer/convert_units.py
@@ -1,6 +1,7 @@
 import pint
 from typing import Callable, Union
 from pint.errors import UndefinedUnitError
+import numpy as np
 
 ureg = pint.UnitRegistry()
 SECONDS = ureg("seconds")
@@ -9,20 +10,26 @@ MICROSECONDS = ureg("microseconds")
 NANOSECONDS = ureg("nanoseconds")
 
 
-def seconds_to_nanoseconds(input_value: Union[float, int]) -> int:
-    return int(input_value * 1_000_000_000)
+def seconds_to_nanoseconds(input_value: Union[float, int, np.ndarray]) -> np.ndarray:
+    return (np.array(input_value) * 1_000_000_000).astype(int)
 
 
-def milliseconds_to_nanoseconds(input_value: Union[float, int]) -> int:
-    return int(input_value * 1_000_000)
+def milliseconds_to_nanoseconds(
+    input_value: Union[float, int, np.ndarray]
+) -> np.ndarray:
+    return (np.array(input_value) * 1_000_000).astype(int)
 
 
-def microseconds_to_nanoseconds(input_value: Union[float, int]) -> int:
-    return int(input_value * 1_000)
+def microseconds_to_nanoseconds(
+    input_value: Union[float, int, np.ndarray]
+) -> np.ndarray:
+    return (np.array(input_value) * 1_000).astype(int)
 
 
-def nanoseconds_to_nanoseconds(input_value: Union[float, int]) -> int:
-    return int(input_value)
+def nanoseconds_to_nanoseconds(
+    input_value: Union[float, int, np.ndarray]
+) -> np.ndarray:
+    return np.array(input_value).astype(int)
 
 
 def get_to_nanoseconds_conversion_method(units: Union[str, bytes]) -> Callable: