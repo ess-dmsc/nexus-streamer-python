99504cac663950d0480474f997dc1ed8c0ce5aa8
Matthew D Jones <matthew.d.jones@tessella.com>
2020-12-25 19:54:16 +0000

    attempt cached, chunked loading of event data

--- nexus_streamer/event_data_source.py
+++ nexus_streamer/event_data_source.py
@@ -28,27 +28,90 @@ class EventDataSource:
             )
             raise BadSource()
 
+        self._event_time_zero = self._group["event_time_zero"][...]
+        self._event_index = self._group["event_index"][...]
+        self._event_index = np.append(
+            self._event_index,
+            np.array([self._group["event_id"].len() - 1]).astype(
+                self._event_index.dtype
+            ),
+        )
+
+        self._tof_buffer_start_index = 0
+        self._event_tof = self._group["event_time_offset"]
+        self._tof_chunk_iter = self._event_tof.iter_chunks()
+        self._current_tof_slice = next(self._tof_chunk_iter)
+        self._tof_buffer = self._event_tof[self._current_tof_slice]
+
+        self._id_buffer_start_index = 0
+        self._event_id = self._group["event_id"]
+        self._id_chunk_iter = self._event_id.iter_chunks()
+        self._current_id_slice = next(self._id_chunk_iter)
+        self._id_buffer = self._event_id[self._current_id_slice]
+
+    def _get_tofs_for_pulse(self, pulse_number: int):
+        start_index = int(
+            self._event_index[pulse_number] - self._tof_buffer_start_index
+        )
+        end_index = int(
+            self._event_index[pulse_number + 1] - self._tof_buffer_start_index
+        )
+
+        tof_for_pulse = np.array([], dtype=self._tof_buffer.dtype)
+        while True:
+            if self._tof_buffer_start_index + self._tof_buffer.size > end_index:
+                return self._convert_event_time(
+                    np.append(
+                        tof_for_pulse,
+                        self._convert_event_time(
+                            self._tof_buffer[start_index:end_index]
+                        ),
+                    )
+                )
+
+            tof_for_pulse = np.append(
+                tof_for_pulse, self._convert_event_time(self._tof_buffer[start_index:])
+            )
+
+            # We need to load more data
+            self._tof_buffer_start_index += self._current_tof_slice[0].stop
+            self._current_tof_slice = next(self._tof_chunk_iter)
+            self._tof_buffer = self._event_tof[self._current_tof_slice]
+
+    def _get_ids_for_pulse(self, pulse_number: int):
+        start_index = int(self._event_index[pulse_number] - self._id_buffer_start_index)
+        end_index = int(
+            self._event_index[pulse_number + 1] - self._id_buffer_start_index
+        )
+
+        ids_for_pulse = np.array([], dtype=self._id_buffer.dtype)
+        while True:
+            if self._id_buffer_start_index + self._id_buffer.size > end_index:
+                return np.append(
+                    ids_for_pulse,
+                    self._id_buffer[start_index:end_index],
+                )
+
+            ids_for_pulse = np.append(ids_for_pulse, self._id_buffer[start_index:])
+
+            # We need to load more data
+            self._id_buffer_start_index += self._current_id_slice[0].stop
+            self._current_id_slice = next(self._id_chunk_iter)
+            self._id_buffer = self._event_id[self._current_id_slice]
+
     def get_data(
         self,
     ) -> Generator[Tuple[Optional[np.ndarray], Optional[np.ndarray], int], None, None]:
         """
         Returns None instead of a data when there is no more data
         """
-        event_time_zero = self._group["event_time_zero"][...]
-        event_index = self._group["event_index"][...]
-        event_index = np.append(
-            event_index,
-            np.array([self._group["event_id"].len() - 1]).astype(event_index.dtype),
-        )
         for pulse_number in range(self._group["event_index"].len()):
-            pulse_time = self._convert_pulse_time(event_time_zero[pulse_number])
-            yield self._convert_event_time(
-                self._group["event_time_offset"][
-                    event_index[pulse_number] : event_index[pulse_number + 1]
-                ]
-            ), self._group["event_id"][
-                event_index[pulse_number] : event_index[pulse_number + 1]
-            ], pulse_time
+            if pulse_number == 82:
+                print("break")
+            pulse_time = self._convert_pulse_time(self._event_time_zero[pulse_number])
+            yield self._get_tofs_for_pulse(pulse_number), self._get_ids_for_pulse(
+                pulse_number
+            ), pulse_time
         yield None, None, 0
 
     def _has_missing_fields(self) -> bool:
